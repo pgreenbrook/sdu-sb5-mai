00771239fbb82ff719d3c60e047d42f5c822f912
Peter Grønbæk Andersen <peter.groenbaek@gmail.com>
2015-11-16 22:09:42 +0100

    Added second working handle for linear gradient

--- jhotdraw7.2/src/org/jhotdraw/draw/GradientHandleKit.java
+++ jhotdraw7.2/src/org/jhotdraw/draw/GradientHandleKit.java
@@ -34,9 +34,10 @@ public class GradientHandleKit {
         
         if(g instanceof LinearGradient) {
             handles.add(new LinearGradientHandleOne(f));
+            handles.add(new LinearGradientHandleTwo(f));
         }
         else if(g instanceof RadialGradient) {
-            handles.add(new GradientHandle2(f));
+            
         }
     }
     
@@ -89,6 +90,69 @@ public class GradientHandleKit {
                 }
                 owner.changed();
             }
+        }
+
+        public void trackEnd(Point anchor, Point lead, int modifiersEx) {
+            // TODO fireUndoableEdit
+        }
+
+        @Override
+        public void draw(Graphics2D g) {
+            drawCircle(g, Color.ORANGE, Color.BLACK);
+        }
+        
+    }
+    
+    private static class LinearGradientHandleTwo extends AbstractHandle {
+        
+        public LinearGradientHandleTwo(Figure owner) {
+            super(owner);
+        }
+
+        protected Rectangle basicGetBounds() {
+            Rectangle r = new Rectangle(locate());
+            r.grow(getHandlesize() / 2 + 1, getHandlesize() / 2 + 1);
+            return r;
+        }
+    
+        private Point locate() {
+            Figure owner = getOwner();
+            LinearGradient g = (LinearGradient) owner.getAttribute(FILL_GRADIENT);
+            
+            Rectangle2D.Double r = owner.getBounds();
+            Point2D.Double p = new Point2D.Double(
+                    r.x + (owner.getPreferredSize().getWidth() * g.getX2()),
+                    r.y + (owner.getPreferredSize().getHeight() * g.getY2())
+                    );
+            if (TRANSFORM.get(owner) != null) {
+                TRANSFORM.get(owner).transform(p, p);
+            }
+            return view.drawingToView(p);
+        }
+
+        public void trackStart(Point anchor, int modifiersEx) {
+            
+        }
+
+        public void trackStep(Point anchor, Point lead, int modifiersEx) {
+            Figure owner = getOwner();
+            LinearGradient g = (LinearGradient) owner.getAttribute(FILL_GRADIENT);
+            
+            Point2D.Double p = view.viewToDrawing(lead);
+            owner.willChange();
+            
+            double x = (p.x - owner.getBounds().x) / owner.getPreferredSize().getWidth();
+            double y = (p.y - owner.getBounds().y) / owner.getPreferredSize().getHeight();
+            g.setGradientVector(g.getX1(), g.getY1(), x, y);
+            
+            if (TRANSFORM.get(owner) != null) {
+                try {
+                    TRANSFORM.get(owner).inverseTransform(p, p);
+                } catch (NoninvertibleTransformException ex) {
+
+                }
+            }
+            owner.changed();
         }
 
         public void trackEnd(Point anchor, Point lead, int modifiersEx) {
